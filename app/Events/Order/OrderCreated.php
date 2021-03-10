accb7b61f68d26dfeff89cbe1e8d8dcfb6f5edce
Eugene Yavgel <eugene.yavgel@gmail.com>
2021-03-10 17:00:56 +0200

    Code cleaning

--- app/Events/Order/OrderCreated.php
+++ app/Events/Order/OrderCreated.php
@@ -3,17 +3,12 @@
 namespace App\Events\Order;
 
 use App\Models\Order;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class OrderCreated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
     public Order $order;
 
